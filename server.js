bf943cb562103dc49dac3f76972c4d5ad0c87b74
Jefferson Freitas <jefferson.si@gmail.com>
2020-05-18 23:20:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/',(req, resp) => {
     resp.status(200).send( { message:"Olá Mundo!" } );
 });
 
+app.get('/teste',(req, resp) => {
+    resp.status(200).send( { message:"Este é um teste!" } );
+});
+
 app.listen(3001, () => {
     console.log("Api rodando na porta 3001");
 });
